70df324ed362e76b65a7542c89084c440fbd5abe
Sharon Oud <mrh_sharon@hotmail.com>
2017-05-03 21:35:28 -0500

    adding table with data for db

--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,22 @@
 CREATE DATABASE plants_db;
 USE plants_db;
 
-CREATE TABLE trees
+CREATE TABLE `plants_db`.`fruit_trees`
 (
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+   `tree_id` INTEGER NOT NULL AUTO_INCREMENT,
+   `common_name` VARCHAR(255) NOT NULL,
+   `cultivar` VARCHAR(255) NULL,
+   `botanical_name` VARCHAR(255) NULL,
+   `ripening_season` VARCHAR(255) NULL,
+   `chill_min` INTEGER NULL,
+   `chill_max` INTEGER NULL,
+   `cold_hardiness` INTEGER NULL,
+   `fruit` VARCHAR(4000) NULL,
+   `water_needs` VARCHAR(4000) NULL,
+   `soil_type` VARCHAR(45) NULL,
+   `fertilizer` VARCHAR(4000) NULL,
+   `originating_region` VARCHAR(255) NULL,
+   `description` VARCHAR(4000) NULL,
+   `parentage` VARCHAR(255) NULL,
+   PRIMARY KEY (`tree_id`)
 );
