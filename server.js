31c6084706df539bf4d9610d6c67d706a4f9d658
Dragon <Dragon>
2017-05-07 17:00:41 -0500

    nuke folders

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// *****************************************************************************
 // Server.js is the initial starting point for the Node/Express server.
 // *** Dependencies
 var express = require("express");
@@ -15,6 +14,7 @@ var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
 // Sets up the Express app to handle data parsing
+app.use(express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -33,11 +33,12 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
-// Import routes and give the server access to them. 
-require("./routes/html-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
+// Import routes and give the server access to them.
+var routes = require("./controllers/catsController.js");
 
+app.use("/", routes);
+
+app.listen(port);
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
