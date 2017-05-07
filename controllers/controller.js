e49a4a7dac1bd55c5e593c623950a344c0c7108c
Karin Rimstidt <Karin Rimstidt>
2017-05-07 18:45:42 -0500

    end of sunday

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,8 +2,8 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
-var cat = require("../models/plantbot.js");
+// Import the model (plants.js) to use its database functions.
+var plants = require("../models/plantbot.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
--- public/javascript/plantbot.js
+++ public/javascript/plantbot.js
@@ -41,14 +41,109 @@ axios.get({url: queryURL})
                     // Appending the image
                     plantDiv.append(image);
 
+
+
                     // Storing the botanicalName data and Creating an element to have the botanicalName displayed
                     var botanicalName = $("<p>").text("Botanical Name: " + results[i].botname);
 
                     // Displaying the botanicalName
-                    plantDiv.append(botanicalName);
+                    botanical.append(botanicalName);
 
-                    // Putting the entire movie above the previous movies
-                    $("#plant-view").prepend(plantDiv);                
+
+                      // Storing the commonName data and Creating an element to have the commonName displayed
+                    var commonName = $("<p>").text("Common Name: " + results[i].tfvname);
+
+                    // Displaying the botanicalName
+                    common.append(commonName);
+
+
+
+                    // add database call for cultivar name
+                    //store and display cultivar
+                    var cultivarEntered
+
+               module.exports = function(app) {
+
+  // Search for Specific Character (or all characters) then provides JSON
+  app.get("/", function(req, res) {
+
+    // If the user provides a specific character in the URL...
+    if (req.params.characters) {
+
+      // Then display the JSON for ONLY that character.
+      // (Note how we're using the ORM here to run our searches)
+      fruit_trees.findOne({
+        where: {
+          routeName: req.params.characters
+        }
+      }).then(function(result) {
+        return res.json(result);
+      });
+    }
+
+    // Otherwise...
+    else {
+      // Otherwise display the data for all of the characters.
+      // (Note how we're using Sequelize here to run our searches)
+      Character.findAll({})
+        .then(function(result) {
+          return res.json(result);
+        });
+    }
+
+  });
+
+
+
+// when user hits the search-btn
+$("#searchBtn").on("click", function() {
+
+  // save the character they typed into the character-search input
+  var searchedCharacter = $("#characterSearch").val().trim();
+
+  // replace any spaces between that character with no space
+  // (effectively deleting the spaces). Make the string lowercase
+  searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
+
+  // run an AJAX GET-request for our servers api,
+  // including the user's character in the url
+  $.get("/api/" + searchedCharacter, function(data) {
+    // log the data to our console
+    console.log(data);
+    // if the data is false (i.e. not there), then return an error message
+    if (data === false) {
+      $("#name").text("The force is not strong with this one. Your character was not found. ");
+      // don't show the stats section, since there are no stats to show
+      $("#stats").hide();
+    }
+    // otherwise
+    else {
+      // show the stats section
+      $("#stats").show();
+      // put the character name in the name tag,
+      $("#name").text(data.name);
+      // the role in the role tag,
+      $("#role").text(data.role);
+      // the age in the age tag,
+      $("#age").text(data.age);
+      // and the force points in the force-points tag
+      $("#forcePoints").text(data.forcePoints);
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+                    var commonName = $("<p>").text("Common Name: " + results[i].tfvname);
+
+                    // Displaying the botanicalName
+                    common.append(commonName);
         }
     }
         });
