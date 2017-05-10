5fcabd4226822d698cc6fa7cde7f3e0ce29d0750
Sharon Oud <mrh_sharon@hotmail.com>
2017-05-09 19:21:27 -0500

    another heroku test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Server.js is the initial starting point for the Node/Express server.
 // *** Dependencies
 var express = require("express");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var axios = require('axios');
@@ -14,8 +15,11 @@ var PORT = process.env.PORT || 8080;
 var db = require("./models");
 
 // Sets up the Express app to handle data parsing
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
 app.use(bodyParser.json());
+//The "extended" syntax allows for rich objects and arrays to be encoded into the URL-encoded format, allowing for a JSON-like experience with URL-encoded. 
+//This object will contain key-value pairs, where the value can be a string or array (when extended is false), or any type (when extended is true).
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
@@ -23,18 +27,13 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
 
-// Static directory
-app.use(express.static(process.cwd() + "/public"));
-
 // Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/controller.js");
 
 app.use("/", routes);
 
