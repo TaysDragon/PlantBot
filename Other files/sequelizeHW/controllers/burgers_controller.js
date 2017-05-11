0ed1832e95156e782435de1df6a1459ab94c1ee6
Dragon <Dragon>
2017-05-11 14:34:17 -0500

    changing to sequelize

--- Other files/sequelizeHW/controllers/burgers_controller.js
+++ Other files/sequelizeHW/controllers/burgers_controller.js
@@ -1,12 +1,14 @@
+// Our Burger controller
+// =====================
 // This file uses Sequelize to manage data manipulation
 // for all apropos http requests.
-
-//Dependencies
+// NOTE: This is the same file from last week's homework,
+// but with each route gutted and replaced with sequelize queries
+// where references to our outmoded ORM file once sat.
 var express = require("express");
 
 var router = express.Router();
-
-// Import the model (plants.js) to use its database functions.
+// grabbing our models
 var db = require("../models");
 
 // get route -> index
