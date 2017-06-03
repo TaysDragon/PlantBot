ee7c87df9e8d23a3c702731c5fac7e3f7a5f069b
Sharon Oud <mrh_sharon@hotmail.com>
2017-06-03 02:34:47 -0500

    working database queries

--- public/js/all.js
+++ public/js/all.js
@@ -1,35 +1,35 @@
-
 // Make a get request to our api route that will return every book
 $.get("/api/all", function(data) {
-  // For each book that our server sends us back
-  for (var i = 0; i < data.length; i++) {
-    // Create a parent div to hold book data
-    var plantSection = $("<div>");
-    // Add a class to this div: 'well'
-    plantSection.addClass("well");
-    // Add an id to the well to mark which well it is
-    plantSection.attr("id", "book-well-" + i);
-    // Append the well to the well section
-    $("#allPlants").append(plantSection);
+    // For each book that our server sends us back
+    for (var i = 0; i < data.length; i++) {
+        // Create a parent div to hold book data
+        var plantSection = $("<div>");
+        // Add a class to this div: 'well'
+        plantSection.addClass("well");
+        // Add an id to the well to mark which well it is
+        plantSection.attr("id", "book-well-" + i);
+        // Append the well to the well section
+        $("#allPlants").append(plantSection);
 
-    // Now  we add our book data to the well we just placed on the page
-    $("#book-well-" + i).append("<h2>" + (i + 1) + ". " + data[i].column_name + "</h2>");
-    $("#book-well-" + i).append("<p><h2>Common name: </h2>" + data[i].common_name + "</p>");
-      $("#book-well-" + i).append("<p>Cultivar: " + data[i].cultivar + "</p>");
-      $("#book-well-" + i).append("<p>Botanical name: " + data[i].botanical_name + "</p>");
-      $("#book-well-" + i).append("<p>Ripening season: " + data[i].ripening_season + "</p>");
-      $("#book-well-" + i).append("<p>Chill min: " + data[i].chill_min + "</p>");
-      $("#book-well-" + i).append("<p>Chill max: " + data[i].chill_max + "</p>");
-      $("#book-well-" + i).append("<p>Cold Hardiness: " + data[i].cold_hardiness + "</p>");
-      $("#book-well-" + i).append("<p>Fruit: " + data[i].fruit + "</p>");
-      $("#book-well-" + i).append("<p>Water needs: " + data[i].water_needs + "</p>");
-      $("#book-well-" + i).append("<p>Sun: " + data[i].sun + "</p>");
-      $("#book-well-" + i).append("<p>Soil type: " + data[i].soil_type + "</p>");
-      $("#book-well-" + i).append("<p>PH low: " + data[i].ph_low + "</p>");
-      $("#book-well-" + i).append("<p>PH high: " + data[i].ph_high + "</p>");
-      $("#book-well-" + i).append("<p>Fertilizer: " + data[i].fertilizer + "</p>");
-      $("#book-well-" + i).append("<p>Originating region: " + data[i].originating_region + "</p>");
-      $("#book-well-" + i).append("<p>Description: " + data[i].description + "</p>");
-      $("#book-well-" + i).append("<p>Parentage: " + data[i].parentage + "</p>");
-  }
+        // Now  we add our book data to the well we just placed on the page
+        $("#book-well-" + i).append("<h2>" + data[i].id + "</h2>");
+        // $("#book-well-" + i).append("<h2>" + data[i].keys + "</h2>");
+        $("#book-well-" + i).append("<p><h2>Common name: </h2>" + data[i].common_name + "</p>");
+        $("#book-well-" + i).append("<p><h2>Cultivar:  </h2>" + data[i].cultivar + "</p>");
+        $("#book-well-" + i).append("<p><h2>Botanical name:  </h2>" + data[i].botanical_name + "</p>");
+        $("#book-well-" + i).append("<p><h2>Ripening season:  </h2>" + data[i].ripening_season + "</p>");
+        $("#book-well-" + i).append("<p><h2>Chill min:  </h2>" + data[i].chill_min + "</p>");
+        $("#book-well-" + i).append("<p><h2>Chill max:  </h2>" + data[i].chill_max + "</p>");
+        $("#book-well-" + i).append("<p><h2>Cold Hardiness:  </h2>" + data[i].cold_hardiness + "</p>");
+        $("#book-well-" + i).append("<p><h2>Fruit:  </h2>" + data[i].fruit + "</p>");
+        $("#book-well-" + i).append("<p><h2>Water needs:  </h2>" + data[i].water_needs + "</p>");
+        $("#book-well-" + i).append("<p><h2>Sun:  </h2>" + data[i].sun + "</p>");
+        $("#book-well-" + i).append("<p><h2>Soil type:  </h2>" + data[i].soil_type + "</p>");
+        $("#book-well-" + i).append("<p><h2>PH low:  </h2>" + data[i].ph_low + "</p>");
+        $("#book-well-" + i).append("<p><h2>PH high:  </h2>" + data[i].ph_high + "</p>");
+        $("#book-well-" + i).append("<p><h2>Fertilizer:  </h2>" + data[i].fertilizer + "</p>");
+        $("#book-well-" + i).append("<p><h2>Originating region:  </h2>" + data[i].originating_region + "</p>");
+        $("#book-well-" + i).append("<p><h2>Description:  </h2>" + data[i].description + "</p>");
+        $("#book-well-" + i).append("<p><h2>Parentage:  </h2>" + data[i].parentage + "</p><hr>");
+    }
 });
--- public/js/search.js
+++ public/js/search.js
@@ -1,47 +1,50 @@
 // Variable to hold returned data
 var plantSearched;
 var results;
+var str;
+
+$(document).ready(function() {
+    //The results section starts hidden and will toggle to show
+    $("#resultsDIV").hide();
+    // When user hits the search-btn
+    $("#search").on("click", function(event) {
+        event.preventDefault();
+
+
+        // Save the plant they typed into the earch input
+        var plantSearched = $("#commonName").val().trim() || $("#botanicalName").val().trim() ||
+        $("#cultivar").val().trim() ||
+        $("#water").val().trim() ||
+        $("#sun").val().trim() ||
+        $("#soil").val().trim();
+        plantSearched = plantSearched;
+        str = plantSearched;
+        console.log("search.js 14 plantSearched = " + plantSearched);
+        toProperCase();
 
 
 
-// When user hits the search-btn
-$("#search").on("click", function(event) {
-    event.preventDefault();
-    $("searchDIV").toggle();
-
-    // Save the plant they typed into the earch input
-    var plantSearched = $("#commonName").val().trim();
-    plantSearched = plantSearched;
-    console.log("search.js 14 plantSearched = " + plantSearched);
-
-    // Make an AJAX get request to our api, including the user's book in the url
-    $.get("/api/" + plantSearched, function(data) {
-        console.log("search.js 19" + data);
-        $.post("/api/" + plantSearched, data);
-        // render function to send search results data to the page
-        renderPlants(data);
-        toggleSearch();
+        // Make an AJAX get request to our api, including the user's book in the url
+        $.get("/api/" + plantSearched, function(data) {
+            console.log("search.js 19" + data);
+            $.post("/api/" + plantSearched, data);
+            // render function to send search results data to the page
+            renderPlants(data);
+            $("#searchDIV, #resultsDIV").toggle();
+        });
     });
 });
 
 
-//The element will not take up any space when the display property set to "none".
-function toggleSearch() {
-    var x = document.getElementById('searchDIV');
-    if (x.style.display === 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
-}
 
-function toggleResults() {
-    var x = document.getElementById('resultsDIV');
-    if (x.style.display === 'none') {
-        x.style.display = 'none';
-    } else {
-        x.style.display = 'block';
-    }
+function toProperCase(){
+temp_arr = str.split('.');
+for (i = 0; i < temp_arr.length; i++) {
+temp_arr[i]=temp_arr[i].trim()
+temp_arr[i] = temp_arr[i].charAt(0).toUpperCase() + temp_arr[i].substr(1).toLowerCase();
+}
+str=temp_arr.join('. ') + '.';
+console.log("toProperCase "+str);
 }
 
 // When user hits the author-search-btn
@@ -80,33 +83,42 @@ function toggleResults() {
 
 function renderPlants(data) {
     if (data.length !== 0) {
-      $("#results").empty();
-      $("#results").show();
-      $.post("/api/plantSearched", data);
-      console.log("search.js 89 " + data)
+        $("#results").empty();
+        $("#results").show();
+        $.post("/api/plantSearched", data);
+        console.log("search.js 89 " + data)
 
-      for (var i = 0; i < data.length; i++) {
-        var div = $("<div>");
-          div.append("<p> " + data[i].common_name + "</p>");
-          div.append("<p> " + data[i].cultivar + "</p>");
-          div.append("<p> " + data[i].botanical_name + "</p>");
-          div.append("<p> " + data[i].ripening_season + "</p>");
-          div.append("<p> " + data[i].chill_min + "</p>");
-          div.append("<p> " + data[i].chill_max + "</p>");
-          div.append("<p> " + data[i].cold_hardiness + "</p>");
-          div.append("<p> " + data[i].fruit + "</p>");
-          div.append("<p> " + data[i].water_needs + "</p>");
-          div.append("<p> " + data[i].sun + "</p>");
-          div.append("<p> " + data[i].soil_type + "</p>");
-          div.append("<p> " + data[i].ph_low + "</p>");
-          div.append("<p> " + data[i].ph_high + "</p>");
-          div.append("<p> " + data[i].fertilizer + "</p>");
-          div.append("<p> " + data[i].originating_region + "</p>");
-          div.append("<p> " + data[i].description + "</p>");
-          div.append("<p> " + data[i].parentage + "</p>");
 
-          $("#results").append(div);
+        for (var i = 0; i < data.length; i++) {
+            var div = $("<div>");
+            if (data[i].common_name === undefined || null) {
+                console.log("No data.");
+            } else {
+
+                div.append("<p> " + data[i].common_name + "</p>");
+
+            }
+            div.append("<p><h2>Cultivar:  </h2> " + data[i].cultivar + "</p>");
+            div.append("<p><h2>Botanical name:  </h2> " + data[i].botanical_name + "</p>");
+            div.append("<p> <h2>Ripening season:  </h2>" + data[i].ripening_season + "</p>");
+            div.append("<p><h2>Chill min:  </h2> " + data[i].chill_min + "</p>");
+            div.append("<p><h2>Chill max:  </h2> " + data[i].chill_max + "</p>");
+            div.append("<p><h2>Cold Hardiness:  </h2> " + data[i].cold_hardiness + "</p>");
+            div.append("<p> <h2>Fruit:  </h2>" + data[i].fruit + "</p>");
+            div.append("<p><h2>Water needs:  </h2> " + data[i].water_needs + "</p>");
+            div.append("<p><h2>Sun:  </h2> " + data[i].sun + "</p>");
+            div.append("<p><h2>Soil type:  </h2> " + data[i].soil_type + "</p>");
+            div.append("<p><h2>PH low:  </h2> " + data[i].ph_low + "</p>");
+            div.append("<p><h2>PH high:  </h2> " + data[i].ph_high + "</p>");
+            div.append("<p> <h2>Fertilizer:  </h2>" + data[i].fertilizer + "</p>");
+            div.append("<p><h2>Originating region:  </h2> " + data[i].originating_region + "</p>");
+            div.append("<p> <h2>Description:  </h2>" + data[i].description + "</p>");
+            div.append("<p> <h2>Parentage:  </h2>" + data[i].parentage + "</p><hr>");
+
+            $("#results").append(div);
         }
+
+
 
         $(".delete").click(function() {
 
@@ -117,15 +129,13 @@ function renderPlants(data) {
             $.post("/api/delete", info)
                 // On success, run the following code
                 .done(function(deldata) {
-                  // Log the data we found
-                  console.log(deldata);
-                  console.log("Deleted Successfully!");
+                    // Log the data we found
+                    console.log(deldata);
+                    console.log("Deleted Successfully!");
                 });
 
             $(this).closest("div").remove();
 
         });
-
     }
-    toggleResults();
-}
+};
