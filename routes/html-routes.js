fdb6866033d3af2d7908bc5bb854cbcb57125711
Sharon Oud <mrh_sharon@hotmail.com>
2017-06-03 03:56:10 -0500

    heroku try

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,27 +11,27 @@ var path = require("path");
 // =============================================================
 module.exports = function(app) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+    // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads index.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-  });
+    // index route loads index.html
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
 
-  // index route loads search.html
-  app.get("/search", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/search.html"));
-  });
+    // index route loads search.html
+    app.get("/search", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/search.html"));
+    });
 
-  // add route loads the add.html page, where users can enter new books to the db
-  app.get("/add", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/add.html"));
-  });
+    // add route loads the add.html page, where users can enter new books to the db
+    app.get("/add", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/add.html"));
+    });
 
-  // all route loads the all.html page, where all books in the db are displayed
-  app.get("/all", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/all.html"));
-  });
+    // all route loads the all.html page, where all books in the db are displayed
+    app.get("/all", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/all.html"));
+    });
 
 
 };
